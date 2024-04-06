d26979e908288c1fd3ccbb9b46ba68d7ab6704e5
macbook <macbook@MacBook-Air-cua-macbook.local>
2024-04-06 20:59:11 +0700

    frontend for buy and sell

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
-
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Copyright from "./components/Copyright";
+import BasicTabs from "./components/TabControl";
+import { Button, Stack } from "@mui/material";
+import { BrowserProvider } from "ethers";
 export default function App() {
+  const [account, setAccount] = React.useState("");
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert("Please install MetaMask first.");
+      return;
+    }
+
+    const provider = new BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+
+    const account = await signer.getAddress();
+    setAccount(account);
+  };
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite.js example
-        </Typography>
-        <ProTip />
+        <Stack direction={"row"} justifyContent={"end"}>
+          <Button onClick={connectWallet} variant="contained">
+            {account
+              ? `${account.slice(0, 7)}...${account.slice(37)}`
+              : "Connect"}
+          </Button>
+        </Stack>
+        <BasicTabs />
         <Copyright />
       </Box>
     </Container>
---
+++ src/constants/actionType.js
@@ -0,0 +1,4 @@
+export const ACTION_TYPE = {
+  BUY: "BUY",
+  SELL: "SELL",
+};
